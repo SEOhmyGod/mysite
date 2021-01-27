af9397c046c76dbde0f3b87b059f3968cef6b862
SEOhmyGod <76707226+SEOhmyGod@users.noreply.github.com>
2021-01-27 19:33:07 +0100

    Update index-page.js
    remove shitey buttons

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -82,31 +82,11 @@ export const IndexPageTemplate = ({
                     <p>{description}</p>
                   </div>
                 </div>
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products"
-                      See all products
-                      style={{
-                        backgroundColor: 'rgb(0, 0, 0)',
-                        color: 'white',
-                      }}>
-                    </Link>
-                  </div>
-                </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
                     Latest stories
                   </h3>
                   <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog"
-                      Read more
-                      style={{
-                        backgroundColor: 'rgb(0, 0, 0)',
-                        color: 'white',
-                      }}>
-                    </Link>
-                  </div>
                 </div>
               </div>
             </div>

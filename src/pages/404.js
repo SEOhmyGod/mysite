08859d7da20dbe1f8fcab56fd5a78f4c4435d478
SEOhmyGod <76707226+SEOhmyGod@users.noreply.github.com>
2021-03-10 23:44:07 +0100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,14 +4,9 @@ import { Helmet } from 'react-helmet'
 
 const NotFoundPage = () => (
   <Layout>
-  <div>
-  <Helmet
-  <meta name="robots" content="noindex" />
-  />
-  </div>
     <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>I once was lost, but now... nope, still gone</h1>
+      <p>No existo, lo siento</p>
     </div>
   </Layout>
 )

2c06ff2a727e481f63028cf707b1a032738adb19
SEOhmyGod <76707226+SEOhmyGod@users.noreply.github.com>
2021-03-10 22:57:59 +0100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import { Helmet } from 'react-helmet'
 const NotFoundPage = () => (
   <Layout>
   <Helmet
-  {noIndex && <meta name="robots" content="noindex" />}
+  <meta name="robots" content="noindex" />
   />
     <div>
       <h1>NOT FOUND</h1>

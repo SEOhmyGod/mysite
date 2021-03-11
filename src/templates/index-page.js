a924118281fe4ade4067cd37dceb3e3c7fcc8f8f
SEOhmyGod <76707226+SEOhmyGod@users.noreply.github.com>
2021-03-11 22:02:03 +0100

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -73,7 +73,7 @@ export const IndexPageTemplate = ({
                     <h1 className="title">{mainpitch.title}</h1>
                   </div>
                   <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
+                    <h4 className="subtitle">{mainpitch.description}</h3>
                   </div>
                 </div>
                 <div className="columns">

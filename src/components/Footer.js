fd17f24ddf43c4fd98ab6b3ea802d5ec144d0ec1
SEOhmyGod <76707226+SEOhmyGod@users.noreply.github.com>
2021-03-24 22:39:43 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,13 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-primary has-text-primary-invite">
-        <div className="content has-text-centered horizontal">
+        <div className="content has-text-centered">
           <a title="twitter" href="https://twitter.com/Difeorleth">
             <img
               className="fas fa-lg"
               src={twitter}
               alt="Difeorleth"
-              style={{ width: '2em', height: '2em' }}
+              style={{ width: '2em', height: '2em', float:centered }}
             />
           </a>
           <a title="linkedin" href="https://www.linkedin.com/in/tom-boyle-92345a17">
@@ -23,7 +23,7 @@ const Footer = class extends React.Component {
               className="fas fa-lg"
               src={linkedin}
               alt="Thomas Boyle"
-              style={{ width: '2em', height: '2em' }}
+              style={{ width: '2em', height: '2em', float:centered }}
             />
           </a>
           <a title="ResearchGate" href="https://www.researchgate.net/profile/Thomas_Boyle2">
@@ -31,7 +31,7 @@ const Footer = class extends React.Component {
               className="fas fa-lg"
               src="/img/RGfavicon-32x32.png"
               alt="Thomas Boyle"
-             style={{ width: '2em', height: '2em',color: 'rgb(242,242,242)' }}
+             style={{ width: '2em', height: '2em',color: 'rgb(242,242,242)', float:centered }}
             />
           </a>
       </div>

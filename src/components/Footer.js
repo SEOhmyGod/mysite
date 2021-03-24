818e0f02dfcb5e5728e57651189e86d95a22a968
SEOhmyGod <76707226+SEOhmyGod@users.noreply.github.com>
2021-03-24 18:46:51 +0100

    give me space

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,35 +12,31 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-primary has-text-primary-invite">
-        <div className="content has-text-centered has-background-primary has-text-primary-invert">
-          <div className="container has-background-primary has-text-primary-invert">
-            <div className="content has-text-centered">
-              <a title="twitter" href="https://twitter.com/Difeorleth">
-                <img
-                  className="fas fa-lg"
-                  src={twitter}
-                  alt="Difeorleth"
-                  style={{ width: '1em', height: '1em' }}
-                />
-              </a>
-              <a title="linkedin" href="https://www.linkedin.com/in/tom-boyle-92345a17">
-                <img
-                  className="fas fa-lg"
-                  src={linkedin}
-                  alt="Thomas Boyle"
-                  style={{ width: '1em', height: '1em' }}
-                />
-              </a>
-              <a title="ResearchGate" href="https://www.researchgate.net/profile/Thomas_Boyle2">
-                <img
-                  className="fas fa-lg"
-                  src="/img/RGfavicon-32x32.png"
-                  alt="Thomas Boyle"
-                 style={{ width: '1em', height: '1em',color: 'rgb(242,242,242)' }}
-                />
-              </a>
-          </div>
-        </div>
+        <div className="content has-text-centered inRow">
+          <a title="twitter" href="https://twitter.com/Difeorleth">
+            <img
+              className="fas fa-lg"
+              src={twitter}
+              alt="Difeorleth"
+              style={{ width: '1em', height: '1em' }}
+            />
+          </a>
+          <a title="linkedin" href="https://www.linkedin.com/in/tom-boyle-92345a17">
+            <img
+              className="fas fa-lg"
+              src={linkedin}
+              alt="Thomas Boyle"
+              style={{ width: '1em', height: '1em' }}
+            />
+          </a>
+          <a title="ResearchGate" href="https://www.researchgate.net/profile/Thomas_Boyle2">
+            <img
+              className="fas fa-lg"
+              src="/img/RGfavicon-32x32.png"
+              alt="Thomas Boyle"
+             style={{ width: '1em', height: '1em',color: 'rgb(242,242,242)' }}
+            />
+          </a>
       </div>
       </footer>
     )

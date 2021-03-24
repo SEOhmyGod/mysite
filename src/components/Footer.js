ad7cbac80a96c6b723a3ebdc6aa2b39bc3608d40
SEOhmyGod <76707226+SEOhmyGod@users.noreply.github.com>
2021-03-24 22:17:08 +0100

    jhgjhfhfhgd

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,8 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import logo from '../img/monkeysee.svg'
-// import facebook from '../img/social/facebook.svg'
-//import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
 import linkedin from '../img/social/linkedin.svg'
-//import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -18,25 +15,23 @@ const Footer = class extends React.Component {
               className="fas fa-lg"
               src={twitter}
               alt="Difeorleth"
-              style={{ width: '1em', height: '1em' }}
+              style={{ width: '2em', height: '2em' }}
             />
           </a>
-          <div></div>
           <a title="linkedin" href="https://www.linkedin.com/in/tom-boyle-92345a17">
             <img
               className="fas fa-lg"
               src={linkedin}
               alt="Thomas Boyle"
-              style={{ width: '1em', height: '1em' }}
+              style={{ width: '2em', height: '2em' }}
             />
           </a>
-          <div></div>
           <a title="ResearchGate" href="https://www.researchgate.net/profile/Thomas_Boyle2">
             <img
               className="fas fa-lg"
               src="/img/RGfavicon-32x32.png"
               alt="Thomas Boyle"
-             style={{ width: '1em', height: '1em',color: 'rgb(242,242,242)' }}
+             style={{ width: '2em', height: '2em',color: 'rgb(242,242,242)' }}
             />
           </a>
       </div>
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
-// import dumpsterfiregif from '../static/img/dumpsterfiregif.gif'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
+
+import dumpsterfiregif from '../static/img/dumpsterfiregif.gif'
 
 export const IndexPageTemplate = ({
   image,
@@ -21,7 +22,7 @@ export const IndexPageTemplate = ({
       className="full-width-image margin-top-0"
       style={{
         backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+          !!image.childImageSharp ? image.childImageSharp.fluid.src : dumpsterfiregif
         })`,
         backgroundPosition: `top left`,
         backgroundAttachment: `fixed`,
